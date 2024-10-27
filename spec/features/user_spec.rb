904891015058c00b6a639ff464e07c148072d5e8
Hauru427 <hauru.051427@gmail.com>
2024-10-28 01:05:37 +0900

    ユーザー登録テストコードの作成

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'ユーザー登録', type: :feature do
+  context "入力情報が正常" do
+    it "ユーザーが新規作成できる" do
+      visit 'user/new'
+      expect{
+        fill_in "ユーザー名",	with: "test"
+        fill_in "メールアドレス", with: "test@example.com"
+        fill_in "パスワード", with: "password"
+        fill_in "パスワード確認", with: "password"
+        click_button "登録"
+      }.to change { User.count }.by(1)
+    end
+  end
+
+  context "入力情報が異常" do
+    it "ユーザーが新規作成に失敗する" do
+      visit 'user/new'
+      expect{
+        fill_in "メールアドレス", with: "test@example.com"
+        click_button "登録"
+      }.to change { User.count }.by(0)
+    end
+  end
+end
