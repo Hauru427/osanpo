b440ea98857628260c3e3876847268f9d469c7c0
Hauru427 <hauru.051427@gmail.com>
2024-10-29 21:19:11 +0900

    Lintエラーの修正

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe 'ユーザー登録', type: :feature do
   context "入力情報が正常" do
     it "ユーザーが新規作成できる" do
       visit new_user_path
-      expect{
+      expect do
         fill_in "ユーザー名",	with: "test"
         fill_in "メールアドレス", with: "test@example.com"
         fill_in "パスワード", with: "password"
         fill_in "パスワード確認", with: "password"
         click_button "登録"
-      }.to change { User.count }.by(1)
+      end.to change { User.count }.by(1)
       # expect(page).to have_content("登録が完了しました")
     end
   end
@@ -18,10 +18,10 @@ RSpec.describe 'ユーザー登録', type: :feature do
   context "入力情報が異常" do
     it "ユーザーが新規作成に失敗する" do
       visit new_user_path
-      expect{
+      expect do
         fill_in "メールアドレス", with: "test@example.com"
         click_button "登録"
-      }.to change { User.count }.by(0)
+      end.to change { User.count }.by(0)
       # expect(page).to have_content("登録に失敗しました")
     end
   end
