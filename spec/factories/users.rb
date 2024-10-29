645504cb922d13e8c3ea0b95efcaef825d2ee652
Hauru427 <hauru.051427@gmail.com>
2024-10-30 02:42:41 +0900

    ログイン機能のテストコードの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,8 @@
-# FactoryBot.define do
-#   factory :user do
-#   end
-# end
+FactoryBot.define do
+  factory :user do
+    name {""}
+    email {"test@example.com"}
+    password {`123456789`}
+    password_confirmation {`123456789`}
+  end
+end
