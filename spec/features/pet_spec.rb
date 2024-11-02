b978e9ce178446fbe0ac6577ded6d2b04b070bfb
Hauru427 <hauru.051427@gmail.com>
2024-11-02 20:46:16 +0900

    テストコードの作成

---
+++ spec/features/pet_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'ペットー登録', type: :feature do
+  context "入力情報が正常" do
+    it "ペットが新規作成できる" do
+      visit new_pet_path
+      expect do
+        fill_in "名前",	with: "test"
+        fill_in "生年月日", with: "2023-01-01"
+        click_button "登録"
+      end.to change { Pet.count }.by(1)
+      expect(page).to have_content("登録が完了しました")
+    end
+  end
+
+  context "入力情報が異常" do
+    it "ペットが新規作成に失敗する" do
+      visit new_pet_path
+      expect do
+        fill_in "生年月日", with: "2023-01-01"
+        click_button "登録"
+      end.to change { Pet.count }.by(0)
+      expect(page).to have_content("登録に失敗しました")
+    end
+  end
+end
